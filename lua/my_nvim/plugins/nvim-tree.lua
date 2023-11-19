d1d341d317b6d95bb4495da3cccec66251869a4a
Ubuntu <asksiti@rs-asksiti-dev.pkc4fplvnalezgdirx4b4adzbb.ix.internal.cloudapp.net>
2023-11-19 10:05:22 +0000

    Added keymappings to nvim-tree to open files in splits.

--- lua/my_nvim/plugins/nvim-tree.lua
+++ lua/my_nvim/plugins/nvim-tree.lua
@@ -4,6 +4,21 @@ return {
 		"nvim-tree/nvim-web-devicons"
 	},
 	config = function()
+		local function my_attach(bufnr)
+			local api = require("nvim-tree.api")
+
+			local function opts(desc)
+				return {desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true}
+			end
+
+			-- default mappings
+			api.config.mappings.default_on_attach(bufnr)
+
+			-- custom mappings
+			vim.keymap.set('n', 'v', api.node.open.vertical, opts('Open: Vertical Split'))
+			vim.keymap.set('n', 'h', api.node.open.horizontal, opts('Open: Horizontal Split'))
+		end
+
 		local nvimtree = require("nvim-tree")
 
 		-- recommended settings from nvim-tree documentation
@@ -15,6 +30,7 @@ return {
 
 		-- configure nvim-tree
 		nvimtree.setup({
+			on_attach = my_attach,
 			view = {
 				width = 40,
 			},
