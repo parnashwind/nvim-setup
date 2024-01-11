188f57d426f2866078dd941b239b13e925179de5
Ubuntu <asksiti@rs-asksiti-dev.pkc4fplvnalezgdirx4b4adzbb.ix.internal.cloudapp.net>
2024-01-11 15:53:04 +0000

    Added Telescope keymap for opening buffers

--- lua/my_nvim/plugins/telescope.lua
+++ lua/my_nvim/plugins/telescope.lua
@@ -26,11 +26,12 @@ return {
 
 		local keymap = vim.keymap -- for conciseness
 
-		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Fuzzy find files in cwd" })
-		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>", { desc = "Fuzzy find recent files" })
-		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<CR>", { desc = "Fuzzy find string in cwd" })
-		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<CR>", { desc = "Fuzzy find string under cursor in cwd" })
-        keymap.set("n", "<leader>fd", "<cmd>Telescope lsp_document_symbols<CR>", { desc = "Find document symbols in buffer"})
+        keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", {desc = "Fuzzy find files in cwd"})
+        keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>", {desc = "Fuzzy find recent files"})
+        keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<CR>", {desc = "Fuzzy find string in cwd"})
+        keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<CR>", {desc = "Fuzzy find string under cursor in cwd"})
+        keymap.set("n", "<leader>fd", "<cmd>Telescope lsp_document_symbols<CR>", {desc = "Find document symbols in buffer"})
+        keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>", {desc = "Show all opened buffers"})
 
 	end
 }
