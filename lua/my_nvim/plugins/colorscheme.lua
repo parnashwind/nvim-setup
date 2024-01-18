3b15cb2cd74d4bec8dbafac748a4a457e3277648
Ubuntu <asksiti@rs-asksiti-dev.pkc4fplvnalezgdirx4b4adzbb.ix.internal.cloudapp.net>
2024-01-18 04:03:16 +0000

    Added some colorschemes

--- lua/my_nvim/plugins/colorscheme.lua
+++ lua/my_nvim/plugins/colorscheme.lua
@@ -1,18 +1,59 @@
 return {
 	{
 		"bluz71/vim-nightfly-colors",
-		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
-			-- load the colorscheme here
-			vim.cmd([[
-				colorscheme nightfly
-				hi Normal ctermbg=None guibg=None
-                hi Visual guibg=#3d3b75 guifg=None
-			]])
-		end,
+        lazy = false,
+		-- priority = 1000, -- make sure to load this before all the other start plugins
+		-- config = function()
+		-- 	-- load the colorscheme here
+		-- 	vim.cmd([[
+		-- 		colorscheme nightfly
+		-- 		hi Normal ctermbg=None guibg=None
+        --      hi NvimTreeNormal ctermbg=None guibg=None
+        --      hi NvimTreeNormalNC ctermbg=None guibg=None
+        --      hi Visual guibg=#3d3b75 guifg=None
+		-- 	]])
+		-- end,
 	},
 	{
 		"folke/tokyonight.nvim",
-		lazy = false,
+		-- lazy = false,
+        priotity = 1000,
+        config = function()
+            vim.cmd([[
+            colorscheme tokyonight
+            hi Normal ctermbg=None guibg=None
+            hi NvimTreeNormal ctermbg=None guibg=None
+            hi NvimTreeNormalNC ctermbg=None guibg=None
+            hi Visual guibg=#3d3b75 guifg=159357
+            ]])
+        end,
 	},
+    {
+        "rebelot/kanagawa.nvim",
+        lazy = false,
+        -- priotity = 1000,
+        -- config = function()
+        --     vim.cmd([[
+        --     colorscheme kanagawa
+        --     hi Normal ctermbg=None guibg=None
+        --     hi NvimTreeNormal ctermbg=None guibg=None
+        --     hi NvimTreeNormalNC ctermbg=None guibg=None
+        --     hi Visual guibg=#3d3b75 guifg=None
+        --     ]])
+        -- end,
+    },
+    {
+        "Mofiqul/dracula.nvim",
+        lazy = false,
+        -- priotity = 1000,
+        -- config = function()
+        --     vim.cmd([[
+        --     colorscheme dracula
+        --     hi Normal ctermbg=None guibg=None
+        --     hi NvimTreeNormal ctermbg=None guibg=None
+        --     hi NvimTreeNormalNC ctermbg=None guibg=None
+        --     hi Visual guibg=#3d3b75 guifg=None
+        --     ]])
+        -- end,
+    },
 }
